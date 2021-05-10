7871cfc09f9ba0431aac7eafc7b7b9018b45d24a
Achraf TAJANI <achraf.tajani-ext@bnf.fr>
2021-05-10 12:51:12 +0200

    forms css

--- css-style/main.js
+++ css-style/main.js
@@ -1,29 +1,36 @@
-const selector = document.querySelector('.custom-select');
+const selector = document.querySelector(".custom-select-1");
 
+selector.addEventListener("change", function (e) {
+  console.log(e.target.value);
+});
 
-selector.addEventListener('change',function(e){
-    console.log(e.target.value)
-})
+selector.addEventListener("mousedown", function (e) {
+  e.preventDefault();
+  const select = selector.children[0];
+  const dropDown = document.createElement("ul");
+  dropDown.className = "selector-options";
 
+  for (var i = 0; i < select.children.length; i++) {
+    const dropDownOption = document.createElement("li");
+    dropDownOption.textContent = select.children[i].textContent;
+    const value = select.children[i].value;
+    dropDownOption.addEventListener("mousedown", function (e) {
+      e.stopPropagation();
+      select.value = value;
+      select.dispatchEvent(new Event("change"));
+      selector.value = value;
+      selector.dispatchEvent(new Event("change"));
+      dropDown.parentNode.removeChild(dropDown);
+    });
+    dropDown.appendChild(dropDownOption);
+  }
+  selector.appendChild(dropDown);
+});
 
-selector.addEventListener('mousedown',function(e){
-    e.preventDefault();
-    const select = selector.children[0]
-    const dropDown  = document.createElement('ul');
-    dropDown.className = 'selector-options';
-    
-    for(var i=0;i<select.children.length;i++){
-        const dropDownOption = document.createElement('li')
-        dropDownOption.textContent = select.children[i].textContent;
-        const value = select.children[i].value;
-        dropDownOption.addEventListener("mousedown",function(e){
-            e.stopPropagation();
-            select.value =value;
-            selector.value = value;
-            dropDown.parentNode.removeChild(dropDown);
-        })
-        dropDown.appendChild(dropDownOption)
-    }
-    selector.appendChild(dropDown)
-
-})
+document.addEventListener("click", function (event) {
+  var dropdwon = document.querySelector(".selector-options");
+  var isClickInsideElement = selector.contains(event.target);
+  if (!isClickInsideElement && dropdwon) {
+    dropdwon.parentNode.removeChild(dropdwon);
+  }
+});
